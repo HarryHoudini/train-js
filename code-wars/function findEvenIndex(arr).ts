fc2e55d5ce7fe2a8ccc5d7b35d0a9d7c52595f76
Harry Houdini <dev.jon.jonson@gmail.com>
2024-06-29 09:58:54 +0300

    Fix

--- code-wars/function findEvenIndex(arr).ts
+++ code-wars/function findEvenIndex(arr).ts
@@ -1,16 +1,3 @@
-function findEvenIndex(arr)
-{
-  var left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
-  for(var i = 0; i < arr.length; i++) {
-      if(i > 0) left += arr[i-1];
-      right -= arr[i];
-
-      if(left == right) return i;
-  }
-
-  return -1;
-}
-
 function findEvenIndex2(arr) {
     let left = 0, right = arr.reduce((acc, cur) => acc + cur, 0)
 
