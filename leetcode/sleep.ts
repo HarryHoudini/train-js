9de864c8b3bbea45286b30c7687b49d2327bc844
Harry Houdini <dev.jon.jonson@gmail.com>
2024-07-06 16:06:00 +0300

    sleep

---
+++ leetcode/sleep.ts
@@ -0,0 +1,24 @@
+// 2621. Sleep
+// https://leetcode.com/problems/sleep/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+
+
+async function sleep(millis: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, millis))
+}
+
+
+// Example 1:
+
+// Input: millis = 100
+// Output: 100
+// Explanation: It should return a promise that resolves after 100ms.
+// let t = Date.now();
+// sleep(100).then(() => {
+//   console.log(Date.now() - t); // 100
+// });
+// Example 2:
+
+// Input: millis = 200
+// Output: 200
+// Explanation: It should return a promise that resolves after 200ms.
