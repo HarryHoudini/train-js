b2947aeed53a37be087d7628ea9a14717c7a1f8e
Harry Houdini <dev.jon.jonson@gmail.com>
2024-07-09 23:36:02 +0300

    fix map

--- leetcode/map.ts
+++ leetcode/map.ts
@@ -1,11 +1,47 @@
-// 2635. Apply Transform Over Each Element in Array
 // https://leetcode.com/problems/apply-transform-over-each-element-in-array/description/?envType=study-plan-v2&envId=30-days-of-javascript
 
+// 2635. Apply Transform Over Each Element in Array
+// Easy
+// Companies
+// Hint
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+
+
+// Example 1:
+
+// Input: arr = [1,2,3], fn = function plusone(n) { return n + 1; }
+// Output: [2,3,4]
+// Explanation:
+// const newArray = map(arr, plusone); // [2,3,4]
+// The function increases each value in the array by one.
 
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    const applyedArr: number[] = []
-    for (let i=1; i <= arr.length; i++) {
-        applyedArr[i-1] = fn(i, i-1)
+    const appliedArr: number[] = []
+    for (let i=0; i < arr.length; i++) {
+         appliedArr[i] = fn(arr[i], i)
     }
-    return applyedArr
+    return appliedArr
 };
+
+
+const map2 = function(arr, fn) {
+    const transformedArr = [];
+    arr.forEach((element, index) => {
+      transformedArr[index] = fn(element, index);
+    });
+    return transformedArr;
+  };
+
+const map3 = function(arr, fn) {
+    return arr.reduce((transformedArr, element, index) => {
+      transformedArr[index] = fn(element, index);
+      return transformedArr;
+    }, []);
+  };
+
+
