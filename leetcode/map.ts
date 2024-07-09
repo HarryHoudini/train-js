21cfa5b8d1a3b4d45d8688c1af7d84a86bed17d5
Harry Houdini <dev.jon.jonson@gmail.com>
2024-07-09 23:36:34 +0300

    create patched mymap

--- leetcode/map.ts
+++ leetcode/map.ts
@@ -45,3 +45,11 @@ const map3 = function(arr, fn) {
   };
 
 
+Array.prototype.mymap = function(callback) {
+    const resultArray = [];
+    for (let index = 0; index < this.length; index++) {
+        resultArray.push(callback(this[index], index, this));
+    }
+    return resultArray;
+}
+
