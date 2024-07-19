82f2a8981ab3f36952f84930df108e4926031a43
Harry Houdini <dev.jon.jonson@gmail.com>
2024-07-20 00:25:07 +0300

    searchInsert

---
+++ leetcode/seachInsert.ts
@@ -0,0 +1,11 @@
+function searchInsert(nums: number[], target: number): number {
+    if (target > nums[nums.length - 1]) {
+        return nums.length
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (target <= nums[i]) {
+            return i
+        }
+
+    }
+};
