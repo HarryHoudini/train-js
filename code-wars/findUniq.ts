900b503ad85bc4198178dc14608eca831546c660
Harry Houdini <dev.jon.jonson@gmail.com>
2024-07-31 23:06:24 +0300

    fix

--- code-wars/findUniq.ts
+++ code-wars/findUniq.ts
@@ -1,1 +1,10 @@
-// x/xx
+export function findUniq(arr: Array<number>): number {
+    arr = arr.sort()
+    return arr[0] == arr[1] ? arr[arr.length - 1] : arr[0]
+}
+
+
+export function findUniq2(arr: Array<number>): number {
+    const x = arr[arr[0] == arr[1] ? 0 : 2];
+    return arr.find(y => y != x);
+  }
