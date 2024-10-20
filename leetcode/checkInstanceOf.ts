50f57dbd377f4cdc4dd92eca2a691dadd13dbc5e
Harry Houdini <dev.jon.jonson@gmail.com>
2024-10-20 18:21:36 +0300

    checkIfInstanceOf fix

--- leetcode/checkInstanceOf.ts
+++ leetcode/checkInstanceOf.ts
@@ -3,19 +3,29 @@
 
 
 function checkIfInstanceOf(obj: any, classFunction: any): boolean {
-    if (Object(classFunction) !== classFunction || obj === null || obj === undefined) return false
+    if (obj === null || obj === undefined) return false;
+    if (Object(classFunction) !== classFunction) return false;
     if (typeof classFunction === 'object' && Object.keys(classFunction).length === 0) return false;
-    return Object(obj) instanceof classFunction
+    return Object(obj) instanceof classFunction;
 };
-
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
  */
-
 
 // () => checkIfInstanceOf(5, Number)
 // () => checkIfInstanceOf([], [])
 
 // () => checkIfInstanceOf([], undefined)
 // () => checkIfInstanceOf(null, [])
+
+
+// const isPrimitive = (val) => Object(val) !== val;
+// isPrimitive(null); // true
+// isPrimitive(undefined); // true
+// isPrimitive(50); // true
+// isPrimitive("Hello!"); // true
+// isPrimitive(false); // true
+// isPrimitive(Symbol()); // true
+// isPrimitive([]); // false
+// isPrimitive({}); // false
