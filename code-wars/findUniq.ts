22b91e6072decd3121e87021fbcd78922881a63c
Harry Houdini <dev.jon.jonson@gmail.com>
2025-07-10 17:54:19 +0300

    feat: add findUniq3 function to identify unique number in an array with validation

--- code-wars/findUniq.ts
+++ code-wars/findUniq.ts
@@ -7,4 +7,19 @@ export function findUniq(arr: Array<number>): number {
 export function findUniq2(arr: Array<number>): number {
     const x = arr[arr[0] == arr[1] ? 0 : 2];
     return arr.find(y => y != x);
-  }
+}
+
+
+export function findUniq3(arr: number[]) : number {
+    if (arr.length < 3 ) {
+        throw new Error("Array must have at least 3 elements");
+    }
+    const [a, b, c] = arr;
+    const common = a === b || a === c ? a : b;
+
+    for (const num of arr) {
+        if (num !== common) {
+            return num;
+        }
+    }
+}
