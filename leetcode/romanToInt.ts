eed8ff628beef62c098929fe8ef3bf1bac0e5250
Harry Houdini <dev.jon.jonson@gmail.com>
2025-07-28 21:24:20 +0300

    feat: implement romanToInt function to convert Roman numerals to integers

---
+++ leetcode/romanToInt.ts
@@ -0,0 +1,34 @@
+function romanToInt(s: string): number {
+    const m = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000,
+    }
+
+    let ans = 0
+
+    for(let i = 0; i < s.length; i++) {
+        let curr = s[i]
+        let curr1 = s[i+1]
+        if(m[curr] < m[curr1]) {
+            ans -= m[curr]
+        } else {
+            ans += m[curr]
+        }
+    }
+    return ans
+
+};
+
+console.log(romanToInt("MCMXCIV")) // 1994
+console.log(romanToInt("LVIII")) // 58
+console.log(romanToInt("III")) // 3
+console.log(romanToInt("IV")) // 4
+console.log(romanToInt("IX")) // 9
+console.log(romanToInt("XLII")) // 42
+console.log(romanToInt("XL")) // 40
+console.log(romanToInt("XLIX")) // 49
