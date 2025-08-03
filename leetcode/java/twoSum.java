879ca4e05fb07cda29a81159e2da99d2e9dbeea2
Harry Houdini <dev.jon.jonson@gmail.com>
2025-08-03 22:54:29 +0300

    fix: correct syntax errors in twoSum function

--- leetcode/java/twoSum.java
+++ leetcode/java/twoSum.java
@@ -3,12 +3,11 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int compliment = target - nums[i]
+            int compliment = target - nums[i];
             if (map.containsKey(compliment)) {
-                return new int[]{map.get(compliment), i};
-            } else {
-                map.put(nums[i], i);
+                return new int[]{map.get(compliment), i}
             }
+            map.put(nums[i], i);
         }
 
         return new int[]{0,0};
