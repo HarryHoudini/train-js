894cdb682c4b997335b697c8c08391b1f1073e1c
Harry Houdini <dev.jon.jonson@gmail.com>
2025-08-03 15:22:12 +0300

    feat: implement secondHighest functions to find the second highest digit in a string

---
+++ leetcode/secondHighest.ts
@@ -0,0 +1,44 @@
+function secondHighest(s: string): number {
+    let first = -1;
+    let second = -1;
+    for (const ch of s) {
+        if ((/\d/).test(ch)) {
+            const char = Number(ch)
+            console.log( second, char)
+            if (char > first) {
+                second = first
+                first = char;
+            }
+            if (char !== first && char > second) {
+                second = char;
+            }
+
+        }
+    }
+    return second;
+}
+function secondHighest2(s: string): number {
+    let first = -1;
+    let second = -1;
+    for (const ch of s) {
+        if (ch >= '0' && ch <= '9') {
+            const char = ch.charCodeAt(0) - 48;
+
+            if (char > first) {
+                second = first
+                first = char;
+            }
+            if (char !== first && char > second) {
+                second = char;
+            }
+        }
+    }
+    return second;
+}
+
+const s = "12321"
+const s2 = "077"
+const s3 = "111"
+// console.log(secondHighest(s)) // 2
+console.log(secondHighest(s2)) // 0
+// console.log(secondHighest(s3)) // -1
