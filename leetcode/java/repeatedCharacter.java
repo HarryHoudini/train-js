9dbaa6de280b88b87e7c5d159627da7714ae635d
Harry Houdini <dev.jon.jonson@gmail.com>
2025-08-04 11:41:43 +0300

    fix: improve formatting and add problem description in repeatedCharacter function

--- leetcode/java/repeatedCharacter.java
+++ leetcode/java/repeatedCharacter.java
@@ -1,9 +1,10 @@
 // https://leetcode.com/problems/first-letter-to-appear-twice/description/
+// 2351. First Letter to Appear Twice
 
 lass Solution {
     public char repeatedCharacter(String s) {
         HashMap<Character,Integer> map = new HashMap<>();
-        for(int i = 0;i<s.length();i++){
+        for (int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
             if (map.containsKey(c)) {
                  return c;
