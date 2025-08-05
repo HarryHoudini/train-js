fd33d7d864112b854230cf253a81e6893eeea232
Harry Houdini <dev.jon.jonson@gmail.com>
2025-08-06 00:02:17 +0300

    feat: add Fibonacci sequence implementation with iterative and recursive methods

---
+++ leetcode/java/fibanaci.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int fib(int n) {
+     if (n < 2) return n;
+     int a = 0, b = 1, sum = 0;
+
+     while (n-- > 1) {
+        sum = a + b;
+        a = b;
+        b = sum;
+     }
+    return sum;
+    }
+}
+
+
+class Solution {
+    public int fib(int n) {
+       int[]dp = new int[n+1];
+       return fibo(n,dp);
+    }
+    public static int fibo(int n, int[]dp){
+        if(n<=1) return n;
+        if(dp[n] != 0) return dp[n];
+        return dp[n] = fibo(n-1,dp)+fibo(n-2,dp);
+    }
+}
