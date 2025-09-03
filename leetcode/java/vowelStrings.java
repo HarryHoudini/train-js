68450235099e113eb426be1d42e437cb4623f302
Harry Houdini <dev.jon.jonson@gmail.com>
2025-09-03 16:21:45 +0500

    feat: add new functionality to enhance user experience

---
+++ leetcode/java/vowelStrings.java
@@ -0,0 +1,14 @@
+import java.util.Set;
+
+
+public class Solution {
+    public int[] vowelStrings(String word, int[][] queries) {
+        Set<char> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        int[] prefixSum = new int[word.length() + 1];
+
+        for (int i=1; i <= word.length(); i++) {
+            int isVowels = vowels.contains(word.charAt(i))l;
+           int prefixSum[i-1] = prefixSum[i] + (isVowels ? 1 : 0);
+        }
+    }
+}
