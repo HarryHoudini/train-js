d34fc4637319dc7050021b53e32eae20d0380804
Harry Houdini <dev.jon.jonson@gmail.com>
2025-09-19 07:40:48 +0300

    feat: add new repeatedSubstringPattern implementation with simplified logic

--- leetcode/java/repeatedSubstringPattern.java
+++ leetcode/java/repeatedSubstringPattern.java
@@ -57,4 +57,35 @@ class Solution {
     }
 }
 
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for(int i=1; i <= n/2; i++){
+            if(n % i == 0){
+                String sub = s.substring(0, i);
+                if (sub.repeat(n / i).equals(s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
 
+
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int len = s.length();
+        for (int i = len / 2; i >= 1; i--) {
+            if (len % i == 0) {
+                String sub = s.substring(0, i);
+                StringBuilder sb = new StringBuilder();
+                sb.append(sub.repeat(len / i));
+                if (sb.toString().equals(s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
